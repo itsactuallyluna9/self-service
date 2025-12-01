e1c3a8903d533e24614c8856f603ead48d8cccbe
thegrumpers <mrya944@gmail.com>
2025-12-01 20:41:09 +0000

    Changed the main.tsx to automatically go to the SignIn page, and installed React Router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import './index.css'
+import SignIn from './pages/SignIn'
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <SignIn/>
+  </React.StrictMode>
 )

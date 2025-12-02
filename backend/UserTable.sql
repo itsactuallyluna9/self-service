ce7f253e6206ec8f0c91f9b51d7f8fd74ed5e918
Adrian Santos <asantos27@cornellcollege.edu>
2025-12-02 13:10:16 -0600

    user table template (without dummy users)

--- backend/UserTable.sql
+++ backend/UserTable.sql
@@ -4,7 +4,7 @@ CREATE TABLE USERS(
     userID AUTO_INCREMENT PRIMARY KEY,
     userName varchar(20) NOT NULL UNIQUE,
     pswd varchar(12) NOT NULL,
-    Utype varchar(20),
+    Utype varchar(20)
 )
 
 

28a6ffad97dcfb34ee4898832b9331380bc58253
Luna <itsactuallyluna9@gmail.com>
2025-12-02 14:21:29 -0600

    Courses Table (#25)
    Co-authored-by: EthynChesn <echesnut26@cornellcollege.edu>

---
+++ backend/sql/coursetable.sql
@@ -0,0 +1,18 @@
+-- Course Data Table
+CREATE TABLE COURSE_DATA(
+    KEYCODE INT PRIMARY KEY AUTO_INCREMENT,
+    -- These Columns are information required for a course, so they cannot be NULL values and must be provided, or pass default values.
+    ACADEMICYEAR SMALLINT NOT NULL, 
+    SEATS TINYINT NOT NULL,
+    COURSECODE CHAR(7) NOT NULL,  --Course Code (Ex. "CSC 318")
+    COURSEBLOCK VARCHAR(15) NOT NULL, -- The Block is a string, so that it may support Adjunct Fall/Spring
+    TITLE VARCHAR(255) NOT NULL,
+    INSCTRUCTORS VARCHAR(255) NOT NULL, --If Multiple, Comma Seperated Instructors
+    CREDITS FLOAT NOT NULL,
+    DEPARTMENT VARCHAR(255) NOT NULL,
+    -- These Columns aren't necessarily required for a course, so they can be NULL values if not provided.
+    FEE SMALLINT,
+    DESCR TEXT,
+    PREREQS TEXT, -- Comma Seperated Prereqs (Ex. "CSC 140, CSC 144, CSC 201, CSC 301")
+    COURSETYPES TEXT -- Comma Seperated Course Types (Ex. "W, Social Science")
+);

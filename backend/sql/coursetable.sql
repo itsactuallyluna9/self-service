04afa5abd0dcc0f047c4efdd9584a1625ba1e759
EthynChesn <echesnut26@cornellcollege.edu>
2025-12-02 13:37:39 -0600

    Update coursetable.sql

--- backend/sql/coursetable.sql
+++ backend/sql/coursetable.sql
@@ -1,10 +1,10 @@
 CREATE TABLE COURSE_DATA(
-    CODE INT PRIMARY KEY AUTO_INCREMENT,  -- Primary Key. Randomized Integer? #Digits?
+    KEYCODE INT PRIMARY KEY AUTO_INCREMENT,  -- Primary Key. Randomized Integer? #Digits?
     -- These Columns are information required for a course, so they cannot be NULL values and must be provided, or pass default values.
     ACADEMICYEAR SMALLINT NOT NULL, 
     SEATS TINYINT NOT NULL,
     COURSECODE CHAR(7) NOT NULL,  --Course Code (Ex. "CSC 318")
-    BLOCKNUM VARCHAR(15) NOT NULL, -- The Block is a string, so that it may support Adjunct Fall/Spring (INT Values but Adjunct are Block 9 and 10?)
+    COURSEBLOCK VARCHAR(15) NOT NULL, -- The Block is a string, so that it may support Adjunct Fall/Spring
     TITLE VARCHAR(255) NOT NULL,
     PROFESSOR VARCHAR(255) NOT NULL,
     CREDITS FLOAT NOT NULL,
@@ -15,5 +15,3 @@ CREATE TABLE COURSE_DATA(
     PREREQS TEXT, -- Comma Seperated Types (Ex. "CSC 140, CSC 144, CSC 201, CSC 301")
     COURSETYPES TEXT -- Comma Seperated Types (Ex. "W, Social Science")
 );
-
-

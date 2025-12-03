61c6d55d315826078a2c966a48f87af128dad1a7
Luna <itsactuallyluna9@gmail.com>
2025-12-03 14:46:38 -0600

    fix lowercase error

--- backend/main.py
+++ backend/main.py
@@ -139,7 +139,7 @@ def get_courses():
 
         return(rows)
 
-    except mariadb.error as e:
+    except mariadb.Error as e:
         print(f'Error connecting to the database: {e}')
         return None
 

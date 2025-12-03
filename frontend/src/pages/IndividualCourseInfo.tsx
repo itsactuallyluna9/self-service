6bed867701abf404b37062840cf613be8d98faf3
thegrumpers <mrya944@gmail.com>
2025-12-03 15:17:55 +0000

    idk why this is making me commit this, i didnt change anything lol

--- frontend/src/pages/IndividualCourseInfo.tsx
+++ frontend/src/pages/IndividualCourseInfo.tsx
@@ -2,9 +2,57 @@ import React from 'react';
 import { useState } from 'react';
 import IndividualCourseTemplate from '../pages/IndividualCourseInfo';
 
+
+
 function IndividualCourseInfo() {
     const [courseID, setCourseID] = useState('');
-
+/*
+    const CourseInfoPageLoader = () => {
+        useEffect(() => {
+            try {
+                const handlePageLoad = () => {
+                    console.log("Page has loaded.");
+                    // Fetch call to backend login API endpoint. CarterLampe 12/1/2025
+                    const response = await fetch('TODO: Enter Course Info Endpoint', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ courseID }),
+                    })
+     
+     
+                    const data = await response.json();
+     
+     
+                    if (response.ok){
+     
+     
+                        if (data.success){
+                            console.log("Course found succesfully.", data)
+                        }
+                       
+                        else {
+                        console.log("Course ID not found..", data);
+                        setError("Course ID not found")
+                        }
+                    }
+            }
+            catch (err) {
+                setError('An error occurred. Please try again.');
+                console.error('Course id error:', err);
+            }
+               
+            };
+            window.addEventListener("load",handlePageLoad)
+     
+     
+            return () => {
+                window.removeEventListener("load",handlePageLoad)
+            }
+        } )
+     
+*/
     return (
         <div>
             <IndividualCourseTemplate 

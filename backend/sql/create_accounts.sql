ce77c43903f8a0076adc811e3f784eda453ac385
EthynChesn <echesnut26@cornellcollege.edu>
2025-12-05 13:51:41 -0600

    Update create_accounts.sql

--- backend/sql/create_accounts.sql
+++ backend/sql/create_accounts.sql
@@ -1,11 +1,12 @@
-INSERT INTO USERS (userName, pswd, usertype) VALUES ("ckawase25", "password123", "STUDENT");
-INSERT INTO USERS (userName, pswd, usertype) VALUES ('asantos27', 'password123', 'STUDENT');
-INSERT INTO USERS (userName, pswd, usertype) VALUES ('clampe27', 'password123', 'STUDENT');
-INSERT INTO USERS (userName, pswd, usertype) VALUES ('echesnut26', 'password123', 'STUDENT');
-INSERT INTO USERS (userName, pswd, usertype) VALUES ('eweber26', 'password123', 'STUDENT');
-INSERT INTO USERS (userName, pswd, usertype) VALUES ('jtressel27', 'password123', 'STUDENT');
-INSERT INTO USERS (userName, pswd, usertype) VALUES ('jwaughon27', 'password123', 'STUDENT');
-INSERT INTO USERS (userName, pswd, usertype) VALUES ('ksaho27', 'password123', 'STUDENT');
-INSERT INTO USERS (userName, pswd, usertype) VALUES ('mryan27', 'password123', 'STUDENT');
-INSERT INTO USERS (userName, pswd, usertype) VALUES ('msmith26', 'password123', 'STUDENT');
-INSERT INTO USERS (userName, pswd, usertype) VALUES ('achavan', 'password123', 'FACULTY');
+INSERT INTO USERS (username, password, usertype) VALUES 
+("ckawase25", "password123", "STUDENT"),
+('asantos27', 'password123', 'STUDENT'),
+('clampe27', 'password123', 'STUDENT'),
+('echesnut26', 'password123', 'STUDENT'),
+('eweber26', 'password123', 'STUDENT'),
+('jtressel27', 'password123', 'STUDENT'),
+('jwaughon27', 'password123', 'STUDENT'),
+('ksaho27', 'password123', 'STUDENT'),
+('mryan27', 'password123', 'STUDENT'),
+('msmith26', 'password123', 'STUDENT'),
+('achavan', 'password123', 'FACULTY');

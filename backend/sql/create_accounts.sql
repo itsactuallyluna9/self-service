9ae1a56e96ff7c0a4b7fbf5967a5fd8c8f50a378
EthynChesn <echesnut26@cornellcollege.edu>
2025-12-05 13:49:49 -0600

    Update create_accounts.sql

--- backend/sql/create_accounts.sql
+++ backend/sql/create_accounts.sql
@@ -1,5 +1,4 @@
-INSERT INTO USERS (userName, pswd, usertype)
-VALUES ("ckawase25", "password123", "STUDENT");
+INSERT INTO USERS (userName, pswd, usertype) VALUES ("ckawase25", "password123", "STUDENT");
 INSERT INTO USERS (userName, pswd, usertype) VALUES ('asantos27', 'password123', 'STUDENT');
 INSERT INTO USERS (userName, pswd, usertype) VALUES ('clampe27', 'password123', 'STUDENT');
 INSERT INTO USERS (userName, pswd, usertype) VALUES ('echesnut26', 'password123', 'STUDENT');

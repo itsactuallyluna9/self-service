2dec43df1d366d796ef402fb082698288f45a4a5
CarterLampe <clampe27@cornellcollege.edu>
2025-12-05 21:18:29 +0000

    Modified the Individual Course Information Page

--- frontend/src/components/IndividualCourseTemplate.tsx
+++ frontend/src/components/IndividualCourseTemplate.tsx
@@ -35,31 +35,38 @@ const IndividualCourseTemplate = (data: CourseProps) => {
         <div className='ind'>
             <form>
                 <img src={Logo} alt="Logo" />
-                <h1>{data.DEPARTMENT}{data.COURSECODE}: {data.TITLE}</h1>
-                <h3>Professor: {data.PROFESSOR}</h3>
-                <h3>Block: {data.BLOCKNUM}</h3>
-                <h4>Seats: {data.SEATS}</h4>
-                <h4>Department: {data.DEPARTMENT}</h4>
-                <h4>Credits: {data.CREDITS}</h4>
-                {data.DESCR !== null && (
-                    <h6>{data.DESCR}</h6>
-                )}
-                {data.FEE !== null && (
-                    <h5>Fees: ${data.FEE}</h5>
-                )}
-                {data.FEE == null && (
-                    <h5>No Fees</h5>
-                )}
-                {data.COURSETYPES !== null && (
-                    <h5>Course Type: {data.COURSETYPES}</h5>
-                )}
-                {data.PREREQS !== null && (
-                    <h5>Prerequisites: {data.PREREQS}</h5>
-                )}
-                {data.PREREQS == null && (
-                    <h5>Prerequisites: None</h5>
-                )}
-                <button onClick={()=>{returnToHome()}}>Back</button>
+                <h2>{data.DEPARTMENT}{data.COURSECODE}: {data.TITLE}</h2>
+                <div className="course-info">
+                    <div className="course-info-left">
+                        <p>
+                        Block: {data.BLOCKNUM} | Year: {data.ACADEMICYEAR}
+                        </p>
+                    </div>
+                    <div className="course-info-right">
+                        <p>Seats: {data.SEATS}</p>
+                        <p>Seats Left: 0 </p>
+                    </div>
+                </div>
+                <div className="instructor-info">
+                    <p>Instructors:</p>
+                    <div className="instructor-label">
+                        <p>{data.PROFESSOR}</p>
+                    </div>
+                </div>
+                <div className="additional-course-info">
+                    <div className="course-description">
+                        <h4>Description:</h4>
+                        <p>{data.DESCR ? data.DESCR : 'None'}</p>
+                    </div>
+                    <div className="course-specs">
+                        <p>Prerequisites: {data.PREREQS ? data.PREREQS : 'None'}</p>
+                        <p>Department: {data.DEPARTMENT} Course Type: {data.COURSETYPES ? data.COURSETYPES : 'None'}</p>
+                        <p>Fees: {data.FEE ? `$${data.FEE}` : 'None'}</p>
+                    </div>
+                </div>
+                <div className="button-container">
+                    <button className="back-button" onClick={()=>{returnToHome()}}>Back</button>
+                </div>
             </form>
         </div>
     )

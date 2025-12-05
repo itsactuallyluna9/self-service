d6dade95c7d52cbbd55fb733f224b4c3ea8df009
EthynChesn <echesnut26@cornellcollege.edu>
2025-12-05 13:56:53 -0600

    Update courses_data.sql

--- backend/sql/tables/courses_data.sql
+++ backend/sql/tables/courses_data.sql
@@ -1,6 +1,6 @@
 -- Course Data Table
 CREATE TABLE COURSE_DATA(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SMALLINT PRIMARY KEY AUTO_INCREMENT,
     -- These Columns are information required for a course, so they cannot be NULL values and must be provided, or pass default values.
     coursecode SMALLINT NOT NULL,   
     title VARCHAR(255) NOT NULL,
@@ -12,3 +12,4 @@ CREATE TABLE COURSE_DATA(
     prereqs TEXT, -- Comma Seperated Prereqs (Ex. "CSC 140, CSC 144, CSC 201, CSC 301")
     coursetypes TEXT -- Comma Seperated Course Types (Ex. "W, Social Science")
 );
+

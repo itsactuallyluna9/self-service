f72f6ea15330625ccd7c65f70fe7af393eef00c1
EthynChesn <echesnut26@cornellcollege.edu>
2025-12-05 13:41:27 -0600

    Create create_accounts.sql

---
+++ backend/create_accounts.sql
@@ -0,0 +1,12 @@
+INSERT INTO USERS (userName, pswd, usertype)
+VALUES ("ckawase25", "password123", "STUDENT");
+INSERT INTO USERS (userName, pswd, usertype) VALUES ('asantos27', 'password123', 'STUDENT');
+INSERT INTO USERS (userName, pswd, usertype) VALUES ('clampe27', 'password123', 'STUDENT');
+INSERT INTO USERS (userName, pswd, usertype) VALUES ('echesnut26', 'password123', 'STUDENT');
+INSERT INTO USERS (userName, pswd, usertype) VALUES ('eweber26', 'password123', 'STUDENT');
+INSERT INTO USERS (userName, pswd, usertype) VALUES ('jtressel27', 'password123', 'STUDENT');
+INSERT INTO USERS (userName, pswd, usertype) VALUES ('jwaughon27', 'password123', 'STUDENT');
+INSERT INTO USERS (userName, pswd, usertype) VALUES ('ksaho27', 'password123', 'STUDENT');
+INSERT INTO USERS (userName, pswd, usertype) VALUES ('mryan27', 'password123', 'STUDENT');
+INSERT INTO USERS (userName, pswd, usertype) VALUES ('msmith26', 'password123', 'STUDENT');
+INSERT INTO USERS (userName, pswd, usertype) VALUES ('achavan', 'password123', 'FACULTY');

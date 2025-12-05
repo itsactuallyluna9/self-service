ca31ca6928bc1a26510683da5999d397553c2711
EthynChesn <echesnut26@cornellcollege.edu>
2025-12-05 13:56:39 -0600

    Update course_offerings.sql

--- backend/sql/tables/course_offerings.sql
+++ backend/sql/tables/course_offerings.sql
@@ -1,9 +1,14 @@
 --Course Offer Table
 CREATE TABLE COURSE_OFFER(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SMALLINT PRIMARY KEY AUTO_INCREMENT,
     academicyear SMALLINT NOT NULL,
     seats TINYINT NOT NULL,
     session VARCHAR(15),
     professor VARCHAR(255) NOT NULL, -- If Multiple, Comma Seperated Instructors
+);
+
+CREATE TABLE COURSES_REL(
+    courseid SMALLINT NOT NULL
+    offerID SMALLINT NOT NULL
 );
 

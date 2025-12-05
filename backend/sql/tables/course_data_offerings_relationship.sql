06162ea27a9ec5c639c4036f5db722d8407d9da7
EthynChesn <echesnut26@cornellcollege.edu>
2025-12-05 14:34:37 -0600

    Delete backend/sql/tables/course_data_offerings_relationship.sql

--- backend/sql/tables/course_data_offerings_relationship.sql
+++
@@ -1,8 +0,0 @@
---Many-to-Many Relationships Table between COURSE_DATA and COURSE_OFFER tables
-CREATE TABLE COURSES_REL(
-    courseid SMALLINT NOT NULL,
-    offerid SMALLINT NOT NULL,
-    PRIMARY KEY (courseid, offerid),
-    CONSTRAINT FOREIGN KEY (courseid) REFERENCES COURSE_DATA(id),
-    CONSTRAINT FOREIGN KEY (offerid) REFERENCES COURSE_OFFER(id)
-);

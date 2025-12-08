24420e42053d6a9bbf02be944dc88c047876db6e
Luna <itsactuallyluna9@gmail.com>
2025-12-08 13:58:39 -0600

    add "its working" test route, tweak path for course details

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from backend import db
 from backend.routes import all_courses, auth, user_courses
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 def create_app():
@@ -16,6 +16,10 @@ def create_app():
     app.register_blueprint(auth.bp, url_prefix='/api')
     app.register_blueprint(all_courses.bp, url_prefix='/api')
     app.register_blueprint(user_courses.bp, url_prefix='/api')
+
+    @app.get('/')
+    def index():
+        return jsonify({'success': True})
 
     db.init_app(app)
     
--- backend/routes/all_courses.py
+++ backend/routes/all_courses.py
@@ -6,28 +6,22 @@ bp = Blueprint('all_courses', __name__)
 
 @bp.get('/courses')
 def get_all_courses():
-    try:
-        with get_db().cursor(dictionary=True) as cursor:
-            cursor.execute("SELECT KEYCODE, ACADEMICYEAR, SEATS, COURSECODE, BLOCKNUM, TITLE, PROFESSOR, CREDITS, DEPARTMENT, FEE FROM COURSE_DATA;")
-            courses = cursor.fetchall()
-            return jsonify({"courses": courses, "success": True})
-    except Exception as e:
-        return jsonify({"error": str(e), "success": False}), 500
+    with get_db().cursor(dictionary=True) as cursor:
+        cursor.execute("SELECT KEYCODE, ACADEMICYEAR, SEATS, COURSECODE, BLOCKNUM, TITLE, PROFESSOR, CREDITS, DEPARTMENT, FEE FROM COURSE_DATA;")
+        courses = cursor.fetchall()
+        return jsonify({"courses": courses, "success": True})
 
 @bp.post('/courses')
 def get_filtered_courses():
     # not implemented yet
     raise NotImplementedError("Filtering courses is not implemented yet.")
 
-@bp.get('/details/<int:course_id>')
+@bp.get('/courses/<int:course_id>')
 def get_course_details(course_id):
-    try:
-        with get_db().cursor(dictionary=True) as cursor:
-            cursor.execute("SELECT * FROM COURSE_DATA WHERE KEYCODE = ?;", (course_id,))
-            if cursor.rowcount == 0:
-                return jsonify({"error": "Course not found", "success": False}), 404
-            course = cursor.fetchone()
-            course['success'] = True
-            return jsonify(course)
-    except Exception as e:
-        return jsonify({"error": str(e), "success": False}), 500
+    with get_db().cursor(dictionary=True) as cursor:
+        cursor.execute("SELECT * FROM COURSE_DATA WHERE KEYCODE = ?;", (course_id,))
+        if cursor.rowcount == 0:
+            return jsonify({"error": "Course not found", "success": False}), 404
+        course = cursor.fetchone()
+        course['success'] = True
+        return jsonify(course)

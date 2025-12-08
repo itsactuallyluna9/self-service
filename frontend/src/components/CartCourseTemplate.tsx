e3401c2c5f42984a2260d4d84e4374fc57b2abc7
thegrumpers <mrya944@gmail.com>
2025-12-08 19:11:56 +0000

    Trying to pull, fixed IndividualCourseTemplate.css to display black text

--- frontend/src/components/CartCourseTemplate.tsx
+++ frontend/src/components/CartCourseTemplate.tsx
@@ -17,18 +17,7 @@ const CartCourseTemplate = (course: CourseData) => {
 
     return (
         <div key={course.KEYCODE} className = 'course-card'>
-                <h2>{course.DEPARTMENT}{course.COURSECODE}: {course.TITLE}</h2>
-                <p>Professor: {course.PROFESSOR}</p>
-                <p>Academic Year: {course.ACADEMICYEAR}</p>
-                <p>Block: {course.BLOCKNUM}</p>
-                <p>Seats Available: {course.SEATS}</p>
-                <p>Credits: {course.CREDITS}</p>
-                {course.FEE !== null && (
-                        <p>Fees: ${course.FEE}</p>
-                    )}
-                    {course.FEE == null && (
-                        <p>No Fees</p>
-                    )}
+                <form><h2>{course.DEPARTMENT}{course.COURSECODE}: {course.TITLE}</h2></form>
         </div>
 )}
 export default CartCourseTemplate

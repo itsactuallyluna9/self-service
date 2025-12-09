e17c89886754b13319318bc1abbf8dd6ae3deed5
169411831 <ckawase@users.noreply.github.com>
2025-12-10 04:35:53 +0900

    modified REGISTERED_COURSES table and added regisitering route

--- backend/sql/tables/registered_table.sql
+++ backend/sql/tables/registered_table.sql
@@ -1,14 +1,14 @@
 CREATE TABLE REGISTERED_COURSES(
-    registeredID INT AUTO_INCREMENT PRIMARY KEY,
+    registeredID INT AUTO_INCREMENT PRIMARY KEY, --leave registeredID INT for now
     userName varchar(50) NOT NULL,
-    keycode INT NOT NULL,
+    keycode SMALLINT NOT NULL, -- This should be SMALLINT since keycode is a foreign key refers id of COURSE_DATA and should mathc the same data type
     enrollmentDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
     FOREIGN KEY (userName) REFERENCES USERS(userName)
         ON UPDATE CASCADE
         ON DELETE CASCADE,
 
-    FOREIGN KEY (keycode) REFERENCES COURSE_DATA(keycode)
+    FOREIGN KEY (keycode) REFERENCES COURSE_DATA(id) --modified keycode to id of COURSE_DATA
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );

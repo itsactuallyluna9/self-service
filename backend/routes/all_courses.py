376ed97220b247295592cf932e370bc7df6c25ed
EthynChesn <echesnut26@cornellcollege.edu>
2025-12-09 12:29:54 -0600

    Update all_courses.py

--- backend/routes/all_courses.py
+++ backend/routes/all_courses.py
@@ -1,6 +1,5 @@
-from flask import Blueprint, jsonify
-
-from backend.db import get_db
+from flask import Blueprint, jsonify, request
+from db import get_db
 
 bp = Blueprint('all_courses', __name__)
 
@@ -29,8 +28,59 @@ def get_all_courses():
 
 @bp.post('/courses')
 def get_filtered_courses():
-    # not implemented yet
-    raise NotImplementedError("Filtering courses is not implemented yet.")
+    data = request.get_json()
+    #search_filter
+    sf = { "semester": data.get('SEMESTER'), "department": data.get('DEPARTMENT'), "professor": data.get('PROFESSOR'), "seats": data.get('AVAILABLE'), "fees": data.get('FEES'), "coursetypes": data.get('ATTRIBUTES'), "session": data.get('BLOCKNUM')}
+    
+    #check how many filters have been applied
+    amount_filters = 0
+    for key in sf:
+        if key:
+            amount_filters += 1
+
+    #filter processing. Output should look something like filters = "session IN ("1", "2", "3", "4", "Adjunct Fall") AND department = "CSC" AND ..."
+    filters = ""
+    for key in sf:
+        if key:
+            if key == "semester":
+                if sf[key] == 'Fall':
+                    filters += 'session IN ("1", "2", "3", "4", "Adjunct Fall")'
+                else:
+                    filters += 'session IN ("5", "6", "7", "8", "Adjunct Sprint")'
+            elif key == "seats" or key == "fees":
+                filters += f"{key} > 0"
+            else:
+                filters += f"{key} = {sf[key]}"
+            if amount_filters > 1:
+                filters += " AND "
+            amount_filters -= 1
+
+    with get_db().cursor(dictionary=True) as cursor:
+            cursor.execute("""
+                SELECT
+                    co.id,
+                    cd.coursecode,
+                    cd.title,
+                    cd.credits,
+                    cd.department,
+                    cd.fee,
+                cd.prereqs,
+                cd.coursetyps,
+                    co.academicyear,
+                    co.session as blocknum
+                    co.professor,
+                    co.seats
+                FROM COURSE_DATA cd
+                LEFT JOIN COURSE_OFFER co ON cd.id = co.courseid;
+                WHERE ?;
+                """,
+                (filters,),
+            )
+            courses = cursor.fetchall()
+            if courses is None:
+                return jsonify({"error": "Courses not found"})
+            return jsonify(courses)
+
 
 @bp.get('/courses/<int:course_id>')
 def get_course_details(course_id):

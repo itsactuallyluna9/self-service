f2b6f307e0610a33690aea9ef18719adec855238
CarterLampe <clampe27@cornellcollege.edu>
2025-12-11 21:24:05 +0000

    Added backend call with empty API endpoint.

--- frontend/src/pages/AddCoursePage.tsx
+++ frontend/src/pages/AddCoursePage.tsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from 'react';
 import {useNavigate, useLocation} from 'react-router'
 import Navbar from '../components/Navbar' 
-import { useCart } from '../components/CartContext';
-import CartTemplate from '../components/CartTemplate'
 
 
 interface CourseData {
@@ -36,12 +34,50 @@ function AddCoursePage() {
       )
     }
 
-    const handleAdd = (data: CourseData) => {
-        setPopupMessage(`${data.department}${data.coursecode} added!`);
+    const handleAdd = async (course: CourseData) => {
+        setPopupMessage(`${course.department}${course.coursecode} added!`);
         setShowPopup(true);
         setTimeout(() => setShowPopup(false), 2000);
+
+        try{
+            // Fetch call to backend login API endpoint. CarterLampe 12/1/2025.
+            const response = await fetch('TODO: Backend API for adding course', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ id: course.id }),
+            })
+            
+            // Dummy response for testing. CarterLampe 12/1/2025.
+            // const response = {
+            //     ok: true,
+            //     json: async () => ({
+            //         success: false,
+            //         token: "test-token",
+            //         message: "Login unsuccessful"
+            //     })
+            // };
+
+            const data = await response.json();
+
+            if (response.ok){
+                
+                if (data.success){
+                    console.log("Adding of course successful.", data)
+        
+                }
+                else {
+                console.log("Adding of course failed.", data);
+                }
+            }
+        }
+        catch (err) {
+            console.error('Adding course error:', err);
+        }
+
         // Prevent adding the same course multiple times. CarterLampe 12/5/2025.
-        setAddedCourses(prev => [...prev, data.id]);
+        setAddedCourses(prev => [...prev, course.id]);
     };
 
   useEffect(() => {

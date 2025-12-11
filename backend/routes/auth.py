62c00037631acd43cdfec7222ccab029dc569a11
Luna <itsactuallyluna9@gmail.com>
2025-12-11 13:40:28 -0600

    add password reset route closes #118

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -21,3 +21,17 @@ def login():
     result = check_user_credentials(username, password)
 
     return jsonify({"success": result})
+
+@bp.post('/reset_password')
+def reset_password():
+    data = request.get_json()
+    
+    username = data.get("username")
+    new_password = data.get("new_password")
+    
+    with get_db().cursor() as cur:
+        query = "UPDATE USERS SET password = ? WHERE username = ?"
+        cur.execute(query, (new_password, username))
+        get_db().commit()
+    
+    return jsonify({"success": True})

3078821159206668384b6829eacfd95beee18747
jwaughon27 <jwaughon27@cornellcollege.edu>
2025-12-12 11:10:26 -0600

    added financial statement and link

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import logo from '../assets/Cornell_logo.png'
 import '../cssFiles/Navbar.css'
 import { useNavigate } from 'react-router'
 import { useState } from 'react';
-import LogoutModal from './LogoutPopup';
+import LogoutModal from './Logout';
 
 const Navbar = () => {
 
@@ -15,8 +15,7 @@ const Navbar = () => {
     }
     const handleConfirmSignOut = () => {
         // Implement signout logic here. CarterLampe 12/5/2025.
-        localStorage.removeItem('UserID')
-        localStorage.removeItem('UserType')
+        localStorage.removeItem('UserId');
         console.log("User signed out.")
         
         return navigate('/SignIn/')
@@ -35,18 +34,10 @@ const Navbar = () => {
             
             <div className="navbar-center">
                 <ul className="nav-links">
-                {/*Add links here for Students */}
-                {localStorage.getItem('UserType') == "STUDENT" && (
-                    <>
-                        <a href="/CourseInformationPage">Courses</a>
-                        <a href="/RegisteredCourses">My Schedule</a>
-                    </>)}
-                {/*Add links here for the Registrar */}
-                {localStorage.getItem('UserType') == "REGISTRAR" && (
-                    <>
-                        <a href="/AddCoursePage">Add Course</a>
-                    </>
-                )}
+                {<><a href="/CourseInformationPage">Courses</a>
+                <a href="/RegisteredCourses">My Schedule</a>
+                <a href="/FinancialStatement">Financial Statement</a></>
+                }
 
                 </ul>
             </div>

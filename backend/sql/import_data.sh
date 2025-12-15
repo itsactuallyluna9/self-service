cde0d64c17196a657d1e5378729fbca4fb7696f6
Luna <itsactuallyluna9@gmail.com>
2025-12-15 13:56:19 -0600

    fix bulk import script

--- backend/sql/import_data.sh
+++ backend/sql/import_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create all tables
-sudo mariadb -p sandbox < tables/create_all_tables.sql
+sudo mariadb sandbox < tables/create_all_tables.sql
 
 # Insert all data  
-sudo mariadb -p sandbox < rawdata/insert_all_data.sql
+sudo mariadb sandbox < rawdata/insert_all_data.sql

de9268178a17b6cd5ce4b673a1af7b9982d67f0e
169411831 <ckawase@users.noreply.github.com>
2025-12-16 05:49:40 +0900

    Update tables

--- backend/sql/tables/student_data.sql
+++ backend/sql/tables/student_data.sql
@@ -1,11 +1,9 @@
 CREATE TABLE STUDENTS(
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  studentid INT PRIMARY KEY,
   fullname VARCHAR(255) NOT NULL, -- leave fullname VARCHAR(255) just in case
-  major VARCHAR(50),
-  minor VARCHAR(50),
-  gradyear SMALLINT NOT NULL, -- graduation year of a student
+  dategranted DATE, -- graduation year of a student
 
-  FOREIGN KEY (id) REFERENCES USERS(userid)
+  FOREIGN KEY (studentid) REFERENCES USERS(userid)
     ON UPDATE CASCADE
     ON DELETE CASCADE
 );

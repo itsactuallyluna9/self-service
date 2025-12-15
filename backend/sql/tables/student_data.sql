84f6325587219cf4af484738d81729f5b828ab0e
169411831 <ckawase@users.noreply.github.com>
2025-12-16 05:52:27 +0900

    change comment on dategranted on STUDENTS

--- backend/sql/tables/student_data.sql
+++ backend/sql/tables/student_data.sql
@@ -1,7 +1,7 @@
 CREATE TABLE STUDENTS(
   studentid INT PRIMARY KEY,
   fullname VARCHAR(255) NOT NULL, -- leave fullname VARCHAR(255) just in case
-  dategranted DATE, -- graduation year of a student
+  dategranted DATE, -- the graduated date if a student is degree awarded otherwise NULL
 
   FOREIGN KEY (studentid) REFERENCES USERS(userid)
     ON UPDATE CASCADE
